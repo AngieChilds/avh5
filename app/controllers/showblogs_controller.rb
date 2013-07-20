f3963ea7b05d405ea0d76232e15a3d6d34e30c3a
angie <pirateprincess_avh@yahoo.com>
2013-07-20 20:56:49 +0200

    rundate

--- app/controllers/showblogs_controller.rb
+++ app/controllers/showblogs_controller.rb
@@ -1,7 +1,7 @@
   class ShowblogsController < ApplicationController
   http_basic_authenticate_with name: "mod", password: "help22", only: [:destroy, :edit]
 # before_filter :find_Showblog
- before_filter :rundate_past
+ before_filter :rundate_past, :only  => :index
      
     def index
         @showblogs = Showblog.all
