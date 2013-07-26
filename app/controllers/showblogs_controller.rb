74fa8a4d8b1ad2c34101905739a5cf13b78704be
angie <pirateprincess_avh@yahoo.com>
2013-07-27 00:08:08 +0200

    delete

--- app/controllers/showblogs_controller.rb
+++ app/controllers/showblogs_controller.rb
@@ -1,7 +1,7 @@
   class ShowblogsController < ApplicationController
   http_basic_authenticate_with name: "mod", password: "help22", only: [:destroy, :edit]
 before_filter :find_Showblog
-before_filter :rundate_past, :only => [:show, :index]
+#before_filter :rundate_past, :only => [:show, :index]
      
     def index
         @showblogs = Showblog.all
