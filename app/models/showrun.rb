3d40827e67df8e3990e131a4f2547e1ca16c025c
angie <pirateprincess_avh@yahoo.com>
2013-07-26 21:12:07 +0200

    order runs

--- app/models/showrun.rb
+++ app/models/showrun.rb
@@ -1,7 +1,6 @@
 class Showrun < ActiveRecord::Base
       belongs_to :showblog
-      default_scope -> {order (':event DESC')}
-
+    
   validates :player, presence: true,
       length: { minimum: 2 }
 end
