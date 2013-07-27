fec974f1169e93746517e9db71920a294545720e
angie <pirateprincess_avh@yahoo.com>
2013-07-27 23:38:29 +0200

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Avh5::Application.routes.draw do
-resources :showblogs  do |showblogs|
-showblogs.resources :showruns, :name_prefix => "showblog"
+resources :showblogs  do 
+resources :showruns, :name_prefix => "showblog"
 end
  
   root 'welcome#index'
