eab3b82f878de57362f734d53db0b01c7543d082
angie <pirateprincess_avh@yahoo.com>
2013-07-28 10:22:32 +0200

    edit destroy showrun

--- app/controllers/showruns_controller.rb
+++ app/controllers/showruns_controller.rb
@@ -27,24 +27,22 @@ http_basic_authenticate_with name: "mod", password: "help22", only: :destroy
 
 
    def show
-   @showblog = Showblog.find(params[:showblog_id])
-     @showrun = @showblog.showruns.find_by_id(params[:id])
+   @showrun = @showblog.showruns(params[:id])
 #showruns#show PATCH  /showblogs/:showblog_id/showruns/:id(.:format)
     redirect_to showblog_path(@showblog)
     end
       def edit
-      @showblog = Showblog.find(params[:showblog_id])
-        @showrun = @showblog.showruns.find(params[:id])
+      @showrun = @showblog.showruns.find(params[:id])
 
         #showruns#edit showblog_showrun GET /showblogs/:showblog_id/showruns/:id(.:format)
 #/showblogs/12/showruns/1/edit
 # edit_showblog_showrun_path
   end
  def update
-    @showblog = Showblog.find(params[:showblog_id])
+  
     @showrun = @showblog.showruns.update(params[@showrun])
 @showrun.update(params[@showrun])
-     redirect_to showblog_path(@showblog)
+     redirect_to showblog_showruns_path(@showblog)
 #/showblogs/:showblog_id/showruns/:id(.:format
   end
 
