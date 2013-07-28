95e17a6a9947e4c18015b297789f979583e9fd98
angie <pirateprincess_avh@yahoo.com>
2013-07-28 11:13:05 +0200

    edit destroy showrun

--- app/controllers/showruns_controller.rb
+++ app/controllers/showruns_controller.rb
@@ -16,6 +16,7 @@ http_basic_authenticate_with name: "mod", password: "help22", only: :destroy
   end
 
   def destroy
+    @showblog = Showblog.find(params[:showblog_id])
     @showrun = @showblog.showruns.find(params[:id])
     @showrun.destroy
      redirect_to showblog_showruns_path(@showblog)
@@ -27,11 +28,13 @@ http_basic_authenticate_with name: "mod", password: "help22", only: :destroy
 
 
    def show
-   @showrun = @showblog.showruns(params[:id])
+     @showblog = Showblog.find(params[:showblog_id])
+      @showrun = @showblog.showruns(params[:id])
 #showruns#show PATCH  /showblogs/:showblog_id/showruns/:id(.:format)
     redirect_to showblog_path(@showblog)
     end
       def edit
+      @showblog = Showblog.find(params[:showblog_id])
       @showrun = @showblog.showruns.find(params[:id])
 
         #showruns#edit showblog_showrun GET /showblogs/:showblog_id/showruns/:id(.:format)
@@ -39,7 +42,7 @@ http_basic_authenticate_with name: "mod", password: "help22", only: :destroy
 # edit_showblog_showrun_path
   end
  def update
-  
+  @showblog = Showblog.find(params[:showblog_id])
     @showrun = @showblog.showruns.update(params[@showrun])
 @showrun.update(params[@showrun])
      redirect_to showblog_showruns_path(@showblog)
