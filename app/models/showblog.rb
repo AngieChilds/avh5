1249ade7c563f2e8d2b91a72c2b3a78747f656de
angie <pirateprincess_avh@yahoo.com>
2013-07-31 11:34:07 +0200

    pernit

--- app/models/showblog.rb
+++ app/models/showblog.rb
@@ -1,6 +1,5 @@
 class Showblog < ActiveRecord::Base
       has_many :showruns, dependent: :destroy
-      accepts_nested_attributes_for :showruns, :allow_destroy => :true
       validates :name, presence: true,
       length: { minimum: 2 }
 end
