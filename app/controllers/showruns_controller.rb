4985e8591f7ea816c2bba4807043743fdd6addae
angie <pirateprincess_avh@yahoo.com>
2013-08-02 20:24:12 +0200

    showrun

--- app/controllers/showruns_controller.rb
+++ app/controllers/showruns_controller.rb
@@ -8,8 +8,7 @@ http_basic_authenticate_with name: "mod", password: "help22", only: [:destroy, :
 
 
   def create
-    @showrun = Showblog.find(params[:showblog_id])
-    @showrun = @showblog.showruns.find(params[:id])
+    @showblog = Showblog.find(params[:showblog_id])
     @showrun = @showblog.showruns.create(params[:showrun].permit(:player, :event, :spec_vote, :level, :make_other, :note_showrun))
     redirect_to showblog_path(@showblog)
     # new_showblog_showrun GET    /showblogs/:showblog_id/showruns/new(.:format)
@@ -27,14 +26,14 @@ http_basic_authenticate_with name: "mod", password: "help22", only: [:destroy, :
  end
    
  def show
-    @showrun = Showblog.find(params[:showblog_id])
+    @showblog = Showblog.find(params[:showblog_id])
     @showrun = @showblog.showruns.find(params[:id])
 #showruns#show PATCH  /showblogs/:showblog_id/showruns/:id(.:format)
     redirect_to showblog_path(@showblog)
 end
 
 def edit
-   @showrun = Showblog.find(params[:showblog_id])
+   @showblog = Showblog.find(params[:showblog_id])
       @showrun = @showblog.showruns.find(params[:id])
 
         #showruns#edit showblog_showrun GET /showblogs/:showblog_id/showruns/:id(.:format)
