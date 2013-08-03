0fc01e5d6a8416757290cef3ba69b8729a4bc84f
angie <pirateprincess_avh@yahoo.com>
2013-08-03 10:01:06 +0200

    showrun

--- app/controllers/showruns_controller.rb
+++ app/controllers/showruns_controller.rb
@@ -34,14 +34,14 @@ end
 
 def edit
    @showblog = Showblog.find(params[:showblog_id])
-   @showrun = @showblog.showrun.find(params[:id])
+   @showrun = showblog.showruns.find(params[:id])
 end
 
  def update 
    @showblog = Showblog.find(params[:showblog_id])
-  @showrun = @showblog.showrun.find(params[:id])
+  @showrun = showblog.showruns.find(params[:id])
    if @showblog.showruns.update(params[:showrun].permit(:player, :event, :spec_vote, :level, :make_other, :note_showrun))
-    redirect_to showblog_path(@showblog)
+    redirect_to showblog_path(@showrun.showblog, @showrun)
 #/showblogs/:showblog_id/showruns/:id(.:format
  else
    render 'edit'
