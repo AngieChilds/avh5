0a02b139c4543906b1550756e632feeba1e78c2c
angie <pirateprincess_avh@yahoo.com>
2013-08-13 21:04:46 +0200

    vote

--- app/helpers/showblogs_helper.rb
+++ app/helpers/showblogs_helper.rb
@@ -1,11 +1,3 @@
 module ShowblogsHelper
-def count_votes
-
-  @showblog = Showblog.find(params[:id])
-  count_votes = Showblog.where(:showblog_id).joins(:showruns).group(:spec_vote).count
-
- "  Vote Results #{count_votes}"
-
-end
 
 end
