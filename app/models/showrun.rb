74ebb231a7268da8ff51419df2c43047637fe3e5
angie <pirateprincess_avh@yahoo.com>
2013-08-17 00:34:51 +0200

    vote

--- app/models/showrun.rb
+++ app/models/showrun.rb
@@ -4,4 +4,8 @@ class Showrun < ActiveRecord::Base
   validates :player, presence: true,
       length: { minimum: 2 }
    attr_accessor :voted
+   def voted
+     @voted = Showblog.where(:showblog_id).joins(:showruns).group(:spec_vote).count {|key, value| puts "#{key} is #{value}" }
+   end
+  
 end
