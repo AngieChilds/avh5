fb9f9ef462657f83a0aea2ccb0d945175849c7c6
AngieChilds <pirateprincess_avh@yahoo.com>
2013-08-18 00:37:04 +0200

    'vote'

--- app/helpers/showruns_helper.rb
+++ app/helpers/showruns_helper.rb
@@ -1,3 +1,13 @@
 module ShowrunsHelper
+# <%  @showblog.showruns.group(:spec_vote).count.each do |spec,count| %>
+ #Spec: <b><%=" #{spec}"%> </b> Votes: <b> <%=" #{count}" %> </b><br/>
+#  <% end %>
 
+  def voted(spec,count)
+  logger.error "-------------------------------------------#{@showblog.inspect}"
+    @showblog.showruns.group(:spec_vote).count.each do |spec,count|
+  " Spec:#{spec} Votes: #{count}"
+  logger.error "------------------------------------------#{@showblog.inspect}"
+    end
+  end
 end
