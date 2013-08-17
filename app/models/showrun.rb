212275a1a0a75a9c79de4397d68c863389d58270
AngieChilds <pirateprincess_avh@yahoo.com>
2013-08-17 11:44:28 +0200

    vote

--- app/models/showrun.rb
+++ app/models/showrun.rb
@@ -5,5 +5,11 @@ class Showrun < ActiveRecord::Base
       length: { minimum: 2 }
    attr_accessor :voted
   
-  
+  def initialize
+
+    @voted = Showblog.where(:showblog_id).joins(:showruns).group(:spec_vote).count
+
+  end
+
+
 end
