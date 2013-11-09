5c018d5399c5519400bd4a971acb2c389769a28a
AngieChilds <pirateprincess_avh@yahoo.com>
2013-11-09 16:51:19 +0200

    master

--- app/controllers/horselists_controller.rb
+++ app/controllers/horselists_controller.rb
@@ -64,7 +64,7 @@ class HorselistsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_horselist
-      @horselist = Request.find(params[:request_id])
+      @Request = Request.find(params[:request_id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
