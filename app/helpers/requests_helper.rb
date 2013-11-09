9578bf91f6d901fe280b9779a2133ad652852cca
AngieChilds <pirateprincess_avh@yahoo.com>
2013-11-09 02:38:46 +0200

    master

--- app/helpers/requests_helper.rb
+++ app/helpers/requests_helper.rb
@@ -1,2 +1,20 @@
 module RequestsHelper
+  def level_count(request)
+  result = ''
+  request.horselists.group(:currant_level).count.each do |level,count|
+    result << "Level: #{level}  Count: #{count} <br/>"
+    end
+  result.html_safe
+end
+def range_count(request)
+  result = ''
+  request.horselists.group(:range).count.each do |range,count|
+    result << "Range: #{level}  Count: #{count} <br/>"
+    end
+  result.html_safe
+end
+
+
+ 
+
 end
