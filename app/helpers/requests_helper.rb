a1975ebc000fddbfa81a0a8e0b0e692660d1cb9a
AngieChilds <pirateprincess_avh@yahoo.com>
2013-11-10 17:08:10 +0200

    master

--- app/helpers/requests_helper.rb
+++ app/helpers/requests_helper.rb
@@ -9,7 +9,7 @@ end
 def range_count(request)
   result = ''
   request.horselists.group(:range).count.each do |range,count|
-    result << "Range: #{range}  Count: #{count} <br/>"
+    result << "Level 1 - 10: #{count} <br/>"
     end
   result.html_safe
 end
