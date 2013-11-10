232ef399b4230aade7c85afb478b4550a5ff06a9
AngieChilds <pirateprincess_avh@yahoo.com>
2013-11-10 11:19:04 +0200

    master

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -1,74 +1,52 @@
 class RequestsController < ApplicationController
-  before_action :set_request, only: [:show, :edit, :update, :destroy]
-
-  # GET /requests
-  # GET /requests.json
-  def index
+   def index
     @requests = Request.all
   end
 
-  # GET /requests/1
-  # GET /requests/1.json
+  
   def show
+    @request = Request.find(params[:id])
   end
 
-  # GET /requests/new
+ 
   def new
     @request = Request.new
   end
 
-  # GET /requests/1/edit
+  
   def edit
+    @request = Request.find(params[:id])
   end
 
-  # POST /requests
-  # POST /requests.json
+  
   def create
-    @request = Request.new(request_params)
+    @request = Request.create(params[:request].permit(:spec, :date, :note))
 
-    respond_to do |format|
       if @request.save
-        format.html { redirect_to @request, notice: 'Request was successfully created.' }
-        format.json { render action: 'show', status: :created, location: @request }
-      else
-        format.html { render action: 'new' }
-        format.json { render json: @request.errors, status: :unprocessable_entity }
-      end
+
+      redirect_to action: :index, id: @request.id
+    else
+      render 'new'
     end
+
+
   end
 
-  # PATCH/PUT /requests/1
-  # PATCH/PUT /requests/1.json
+ 
   def update
-    respond_to do |format|
-      if @request.update(request_params)
-        format.html { redirect_to @request, notice: 'Request was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: 'edit' }
-        format.json { render json: @request.errors, status: :unprocessable_entity }
-      end
+    @request = Request.find(params[:id])
+       if @request.update(params[:request].permit(:spec, :date, :note))
+        redirect_to action: :index, id: @request.id
+        else
+        render 'edit'
     end
   end
 
-  # DELETE /requests/1
-  # DELETE /requests/1.json
+  
   def destroy
+    @request = Request.find(params[:id])
     @request.destroy
-    respond_to do |format|
-      format.html { redirect_to requests_url }
-      format.json { head :no_content }
+   redirect_to requests_url
     end
-  end
-
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_request
-      @request = Request.find(params[:id])
-    end
-
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def request_params
-      params.require(:request).permit(:spec, :date, :note)
-    end
+    
 end
