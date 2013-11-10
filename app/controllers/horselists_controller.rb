2073f5b77feed5c3d66d001b50cc5ce7cad84b48
AngieChilds <pirateprincess_avh@yahoo.com>
2013-11-10 16:23:04 +0200

    master

--- app/controllers/horselists_controller.rb
+++ app/controllers/horselists_controller.rb
@@ -1,8 +1,16 @@
 class HorselistsController < ApplicationController
- 
+
+
   def index
     @horselist = @request.horselists.all
   end
+ def create
+    @request = Request.find(params[:request_id])
+    @horselist = @request.horselists.create(params[:horselist].permit(:player, :horse, :currant_level, :range, :range1, :range2, :range3, :range4, :range5, :range6, :range7, :range6, :range7, :range8, :range9, :range10, :date, :note ))
+ redirect_to request_path(@request)
+  end
+
+
 
   
   def show
@@ -12,11 +20,7 @@ class HorselistsController < ApplicationController
   end
 
    
-  def create
-    @request = Request.find(params[:request_id])
-    @horselist = @request.horselists.create
- redirect_to request_path(@request)
-  end
+ 
 
   def destroy
  @request = Request.find(params[:request_id])
