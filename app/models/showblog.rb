b0bbbf2349649a39ff6107ac0c9a2a2b19781ff6
AngieChilds <pirateprincess_avh@yahoo.com>
2013-11-16 00:02:07 +0200

    showrun

--- app/models/showblog.rb
+++ app/models/showblog.rb
@@ -1,5 +1,5 @@
 class Showblog < ActiveRecord::Base
-      has_many :showruns, -> { order(:event => :desc) }, dependent: :destroy
+      has_many :showruns, dependent: :destroy
       validates :name, presence: true,
       length: { minimum: 2 }
   
