7fa5f5f9ad9d85b899cc3d76fed476d438cce15a
AngieChilds <pirateprincess_avh@yahoo.com>
2013-11-16 13:16:07 +0200

    showrun

--- app/models/showblog.rb
+++ app/models/showblog.rb
@@ -1,6 +1,7 @@
 class Showblog < ActiveRecord::Base
       has_many :showruns, dependent: :destroy
-      validates :name, presence: true,
+      accepts_nested_attributes_for :showruns
+  validates :name, presence: true,
       length: { minimum: 2 }
   
 end
