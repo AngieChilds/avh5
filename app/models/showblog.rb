b743320c8be1b25ebe0ef5350bc6bfc7dcc3234c
AngieChilds <pirateprincess_avh@yahoo.com>
2013-11-17 12:35:27 +0200

    child

--- app/models/showblog.rb
+++ app/models/showblog.rb
@@ -1,6 +1,6 @@
 class Showblog < ActiveRecord::Base
       
-  has_many :showruns, dependent: :destroy
+  has_many :showruns.includes(:event).order(' showrun.event DESC '), dependent: :destroy
       accepts_nested_attributes_for :showruns
   validates :name, presence: true,
       length: { minimum: 2 }
