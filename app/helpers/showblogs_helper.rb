ba2f13cf356d3791b75d645fc650600d25a2f827
AngieChilds <pirateprincess_avh@yahoo.com>
2013-11-17 16:29:48 +0200

    child

--- app/helpers/showblogs_helper.rb
+++ app/helpers/showblogs_helper.rb
@@ -1,12 +1,13 @@
 module ShowblogsHelper
 def vote_spec(showblog)
   result = ''
-  showblog.showruns.group(:spec_vote).order('event ASC').count.each do |spec,count|
+  showblog.showruns.group(:spec_vote, :event).order('event asc').count(:spec_vote).each do |spec,count|
     result << "Spec: #{spec}  Votes: #{count} <br/>"
     # or the other logic you want to add such as using a table or div
   end
   result.html_safe
 end
+
  end
   
 
